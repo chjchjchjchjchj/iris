e47edd4bac340919143aec2f99d0cb3dad8591bd
lr-2002 <2629651228@qq.com>
2024-01-16 13:33:48 +0800

    using homegrid

--- src/envs/wrappers.py
+++ src/envs/wrappers.py
@@ -17,6 +17,7 @@ def make_atari(id, size=64, max_episode_steps=None, noop_max=30, frame_skip=4, d
         env = gym.make(id)
     print('using env in make_atari', id)
     print('the info of the env is ', env)
+    print('spec.id ', env.spec.id)
     assert 'NoFrameskip' in env.spec.id or 'Frameskip' not in env.spec
     env = ResizeObsWrapper(env, (size, size))
     print('after resize, the env info is as: ', env)
