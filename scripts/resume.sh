68a7e30ef2567b835cbbc2a449d54f4b6aa08b5c
lr-2002 <2629651228@qq.com>
2024-01-21 13:42:52 +0800

    feat: resume ok

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,1 +1,2 @@
 python src/main.py common.resume=True hydra.output_subdir=null hydra.run.dir=.
+#python src/main.py env.train.id=homegrid-task common.resume=True hydra.output_subdir=null hydra.run.dir=.
