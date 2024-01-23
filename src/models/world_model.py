184fd2cd10d6f9bca3c2bf73384da0a902d32e7a
lr-2002 <2629651228@qq.com>
2024-01-23 22:08:47 +0800

    fix: change the reward

--- src/models/world_model.py
+++ src/models/world_model.py
@@ -132,6 +132,8 @@ class WorldModel(nn.Module):
         assert torch.all(ends.sum(dim=1) <= 1)  # at most 1 done
         mask_fill = torch.logical_not(mask_padding)
         labels_observations = rearrange(obs_tokens.masked_fill(mask_fill.unsqueeze(-1).expand_as(obs_tokens), -100), 'b t k -> b (t k)')[:, 1:]
-        labels_rewards = (rewards.sign() + 1).masked_fill(mask_fill, -100).long()  # Rewards clipped to {-1, 0, 1}
+        labels_rewards = (rewards * 2 ).masked_fill(mask_fill, -100).long()  # Rewards clipped to {-1, 0, 1}
         labels_ends = ends.masked_fill(mask_fill, -100)
+        # print("real rewards", rewards)
+        # print('labels rewars', labels_rewards)
         return labels_observations.reshape(-1), labels_rewards.reshape(-1), labels_ends.reshape(-1)
